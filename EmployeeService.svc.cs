11d0fa06e793b1caaf62f12351a9970e1c7549f6
ntdat1597 <53183723+ntdat1597@users.noreply.github.com>
2020-11-18 10:13:05 +0700

    Add project files.

---
+++ EmployeeService.svc.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+        EmployeeDataDataContext data = new EmployeeDataDataContext();
+
+        public bool createDepartment(Department newDepartment)
+        {
+            try
+            {
+                data.Departments.InsertOnSubmit(newDepartment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool createEmployee(Employee newEmployee)
+        {
+            try
+            {
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool deleteDepartment(string id)
+        {
+            try
+            {
+                var department = data.Departments.Where(c => c.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(department);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool deleteEmployee(string id)
+        {
+            try
+            {
+                var employee = data.Employees.Where(c => c.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool editDepartment(string id, Department newDepartment)
+        {
+            try
+            {
+                var department = data.Departments.Where(c => c.DepartmentID == int.Parse(id)).FirstOrDefault();
+                data.Departments.DeleteOnSubmit(department);
+                data.Departments.InsertOnSubmit(newDepartment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool editEmployee(string id, Employee newEmployee)
+        {
+            try
+            {
+                var employee = data.Employees.Where(c => c.EmployeeID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Department> GetDepartments()
+        {
+            try
+            {
+                var department = (from Department in data.Departments select Department).ToList();
+                return department;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            try
+            {
+                var employee = (from Employee in data.Employees select Employee).ToList();
+                return employee;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ IEmployeeService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Services;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        // hiển thị
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/GetListEmployee")]
+        List<Employee> GetEmployees();
+
+        // them
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/CreateEmployee")]
+
+        bool createEmployee(Employee newEmployee);
+
+
+        //sua
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/EditEmployee{id}")]
+
+        bool editEmployee(string id, Employee newEmployee);
+
+        //xoa
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/DeleteEmployee{id}")]
+
+        bool deleteEmployee(string id);
+
+
+        // -- Department
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/GetListDepartment")]
+        List<Department> GetDepartments();
+
+        // them
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/CreateDepartment")]
+
+        bool createDepartment(Department newDepartment);
+
+
+        //sua
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/EditDepartment{id}")]
+
+        bool editDepartment(string id, Department newDepartment);
+
+        //xoa
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+           RequestFormat = WebMessageFormat.Json,
+           ResponseFormat = WebMessageFormat.Json,
+           BodyStyle = WebMessageBodyStyle.Bare,
+           UriTemplate = "api/v1/DeleteDepartment{id}")]
+
+        bool deleteDepartment(string id);
+
+    }
+}
